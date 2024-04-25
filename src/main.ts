6fb1e98e51de2fe06f36fb683216b9a87df26ef6
Mohit Dubey <mohit.dubey@mursion.com>
2024-04-25 14:11:44 +0530

    Implemented main functionality: Clicking the button executes the counter function from factory.ts and updates the value displayed. Updated main.ts to handle changes in start_at and step controls by creating a new instance of the factory with updated arguments. All tests pass.

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,22 @@
 import { factory } from "./factory";
 
 let count = factory(0, 1);
-
-function update_count_and_reset_counter() {}
-
-const start_at_control = document.getElementById(
-  "start_at",
-) as HTMLInputElement;
-
+const start_at_control = document.getElementById("start_at") as HTMLInputElement;
 const step_control = document.getElementById("step") as HTMLInputElement;
+const count_button = document.querySelector(".count_button") as HTMLButtonElement;
+const current_count = document.querySelector(".current_count") as HTMLSpanElement;
 
-start_at_control?.addEventListener("change", () => {});
+function update_count_and_reset_counter() {
+  count = factory(parseInt(start_at_control.value), parseInt(step_control.value));
+  update_count();
+}
 
-step_control?.addEventListener("change", () => {});
+start_at_control?.addEventListener("change", update_count_and_reset_counter);
+step_control?.addEventListener("change", update_count_and_reset_counter);
 
-const count_button = document.querySelector(
-  ".count_button",
-) as HTMLButtonElement;
-
-const current_count = document.querySelector(
-  ".current_count",
-) as HTMLSpanElement;
-
-function update_count() {}
+function update_count() {
+  const value = count();
+  current_count.textContent = value.toString();
+}
 
 count_button.addEventListener("click", update_count);
